69d87cb48957ba352137160e8268f3b683fba311
AlejandroAlvarez <tec.alejandroalvarez@gmail.com>
2019-05-27 19:20:41 -0300

    Se modifico la clase buscador

--- TpAterrizar_etapa2/src/modelo/AerolineaLanchita.java
+++ TpAterrizar_etapa2/src/modelo/AerolineaLanchita.java
@@ -3,6 +3,6 @@ package modelo;
 import java.util.ArrayList;
 
 public interface AerolineaLanchita {
-	public ArrayList<String> asientosDisponibles(String origen,String fechaSalida,String horaSalida,String destino,String fechaLlegada,String horaLlegada);
+	public ArrayList<ArrayList<String>> asientosDisponibles(String origen,String fechaSalida,String horaSalida,String destino,String fechaLlegada,String horaLlegada);
 	public void comprar(String codigoAsiento);
 }
