a01e232feedd36ae6487126f528a8c1dc997dc8a
Oscar <oskar.pererya@gmail.com>
2019-07-05 18:37:43 -0300

    05/07 18:38

--- TpAterrizar_etapa2/test/test/TestBuscador.java
+++ TpAterrizar_etapa2/test/test/TestBuscador.java
@@ -35,48 +35,11 @@ public class TestBuscador {
 		ArrayList<ArrayList<String>> asientosLanchita = new LanchitaDummy().getAsientos();
 		when(mockLanchita.asientosDisponibles(ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(asientosLanchita);
 		UsuarioEstandar usuario = new UsuarioEstandar("Alejando","40135297");
-		Busqueda busqueda = new Busqueda(null,null, null, null, null,null,null,null, null, null);
+		Busqueda busqueda = new Busqueda("","", "", "", "","",null,null, null, null);
 		ArrayList<Asiento> asientosEncontrados = buscador.busqueda(usuario, busqueda);
-		System.out.println(asientosLanchita.size());
-		System.out.println(asientosEncontrados.size());
-		System.out.println(new LanchitaDummy().getAsientos().size());
 		Assert.assertEquals("La busqueda no acepta atributos nulos",new LanchitaDummy().getAsientos().size(),asientosEncontrados.size());
 	}
 	
-	/*private ArrayList<ArrayList<String>> lanchitaAsientos(){
-		ArrayList<ArrayList<String>> asientosLanchita = new ArrayList<ArrayList<String>>();
-		
-		ArrayList<String> asiento1 = new ArrayList<String>();
-		ArrayList<String> asiento2 = new ArrayList<String>();
-		
-		asiento1.add("EC0344-42");
-		asiento1.add("56500.60");
-		asiento1.add("P");
-		asiento1.add("P");
-		asiento1.add("D");
-		asiento1.add("18/06/2019");
-		asiento1.add("25/06/2019");
-		
-		asiento2.add("EC0344-66");
-		asiento2.add("12000.30");
-		asiento2.add("P");
-		asiento2.add("P");
-		asiento2.add("D");
-		asiento2.add("18/06/2019");
-		asiento2.add("25/06/2019");
-
-		asientosLanchita.add(asiento1);
-		asientosLanchita.add(asiento2);
-		return asientosLanchita;
-	}
-	private ArrayList<Asiento> AsientosEsperados(){
-		ArrayList<Asiento> asientosEsperados = new ArrayList<Asiento>();
-		Asiento asiento1 = new Asiento("EC0344-42",56500.60,TipoClaseAsiento.PRIMERA,TipoUbicacionAsiento.PASILLO,true,18062019,25062019,new Aerolinea(mockLanchita));
-		Asiento asiento2 = new Asiento("EC0344-66",12000.30,TipoClaseAsiento.PRIMERA,TipoUbicacionAsiento.PASILLO,true,18062019,25062019);
-
-		asientosEsperados.add(asiento1);
-		asientosEsperados.add(asiento2);
-		return asientosEsperados;
-	}*/
+0
 }
 
